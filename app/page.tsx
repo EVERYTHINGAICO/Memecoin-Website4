f1934c4db0fa96b4233b33b8dd00add95eb5e499
reimon <reimonsk8@gmail.com>
2025-05-12 18:02:47 -0700

    tool working

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,40 @@
 "use client";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { motion } from "framer-motion";
-import config, { tokenCA } from "@/config";
 import toast from "react-hot-toast";
 
 export default function HomePage() {
+  const [data, setData] = useState<null | {
+    ca: string;
+    twitter_link: string;
+    telegram_link: string;
+    meta_info: string[];
+  }>(null);
+
+
+  useEffect(() => {
+    fetch("https://re9tawzde2.execute-api.us-west-1.amazonaws.com/stage1/API/FetchData")
+      .then((res) => res.json())
+      .then((resJson) => {
+        try {
+          const parsedBody = JSON.parse(resJson.body);
+          if (Array.isArray(parsedBody) && parsedBody.length > 0) {
+            setData(parsedBody[0]);
+          } else {
+            toast.error("No data found in body");
+          }
+        } catch (e) {
+          toast.error("Failed to parse body JSON");
+        }
+      })
+      .catch(() => toast.error("Failed to fetch data"));
+  }, []);
+
   function playDuckSound() {
     const audio = new Audio("/sounds/duck.mp3");
     audio.play();
   }
+
   return (
     <motion.div
       initial={{ opacity: 0 }}
@@ -27,29 +53,29 @@ export default function HomePage() {
           </div>
           <div className="flex flex-col mt-[52px] md:ml-9 lg:mt-0 text-start items-start justify-center">
             <h1 className="font-bold leading-10 md:leading-normal text-[36px] lg:text-[42px] xl:text-[46px]">
-              {config.homeMainSection.title}
+              Welcome to the Duck App
             </h1>
             <h2 className="font-medium leading-7 mt-1 md:max-w-[400px] text-xl">
-              {config.homeMainSection.description
-                .split("\n")
-                .map((line: any, index: any) => (
-                  <React.Fragment key={index}>
-                    {line}
-                    <br />
-                  </React.Fragment>
-                ))}
+              Get contract info, social links and useful metadata below!
             </h2>
             <div className="mt-3.5 flex flex-row items-center justify-center space-x-2">
-              {config.homeMainSection.buttons.map(
-                (button: any, index: number) => (
-                  <a
-                    key={index}
-                    href={button.href || "about:blank"}
-                    className="font-semibold px-4 border border-[#9e782c] shadow-[2px_2px_0_#9e782c] active:shadow-[0.5px_0.5px_0_#9e782c] active:translate-x-[1px] active:translate-y-[1px] text-white active:scale-[0.99] transition-all ease-linear duration-100 hover:bg-yellow-500/80 active:bg-yellow-500/90 bg-yellow-500/70 text-[17px] py-2.5 rounded-xl"
-                  >
-                    {button.text}
-                  </a>
-                )
+              {data?.twitter_link && (
+                <a
+                  href={data.twitter_link}
+                  target="_blank"
+                  className="font-semibold px-4 border border-[#9e782c] shadow-[2px_2px_0_#9e782c] active:shadow-[0.5px_0.5px_0_#9e782c] active:translate-x-[1px] active:translate-y-[1px] text-white active:scale-[0.99] transition-all ease-linear duration-100 hover:bg-yellow-500/80 active:bg-yellow-500/90 bg-yellow-500/70 text-[17px] py-2.5 rounded-xl"
+                >
+                  Twitter
+                </a>
+              )}
+              {data?.telegram_link && (
+                <a
+                  href={data.telegram_link}
+                  target="_blank"
+                  className="font-semibold px-4 border border-[#9e782c] shadow-[2px_2px_0_#9e782c] active:shadow-[0.5px_0.5px_0_#9e782c] active:translate-x-[1px] active:translate-y-[1px] text-white active:scale-[0.99] transition-all ease-linear duration-100 hover:bg-yellow-500/80 active:bg-yellow-500/90 bg-yellow-500/70 text-[17px] py-2.5 rounded-xl"
+                >
+                  Telegram
+                </a>
               )}
             </div>
           </div>
@@ -62,26 +88,33 @@ export default function HomePage() {
             />
           </div>
         </section>
-        {tokenCA && (
+
+        {data && (
           <section className="w-full flex mb-5 lg:mb-0 lg:mt-0 border-t border-zinc-200 flex-col lg:flex-row items-center justify-between lg:px-[111px] px-5 xl:px-[190px]">
             <div className="flex my-36 xl:mx-9 w-full flex-col items-center justify-center">
-              <div className="font-semibold flex flex-row items-center justify-center text-center space-x-5 relative px-6 border border-[#9e782c] shadow-[1px_1px_0_#9e782c] transition-all ease-linear duration-100 bg-yellow-50 text-black text-[17px] py-3 rounded-xl">
-                <span>Contract Address</span>
-                <a
-                  onClick={() => {
-                    navigator.clipboard
-                      .writeText(tokenCA)
-                      .then(() => {
-                        toast.success("Copied to clipboard");
-                      })
-                      .catch((err) => {
-                        toast.error("Could not copy");
-                      });
-                  }}
-                  className="font-semibold cursor-pointer px-4 uppercase border border-[#9e782c] shadow-[2px_2px_0_#9e782c] active:shadow-[0.5px_0.5px_0_#9e782c] active:translate-x-[1px] active:translate-y-[1px] text-white active:scale-[0.99] transition-all ease-linear duration-100 hover:bg-yellow-500/80 active:bg-yellow-500/90 bg-yellow-500/70 text-[17px] py-2 rounded-xl"
-                >
-                  COPY
-                </a>
+              <div className="font-semibold flex flex-col items-center justify-center text-center space-y-4 relative px-6 border border-[#9e782c] shadow-[1px_1px_0_#9e782c] transition-all ease-linear duration-100 bg-yellow-50 text-black text-[17px] py-6 rounded-xl">
+                <div className="flex flex-row items-center space-x-4">
+                  <span>Contract Address:</span>
+                  <button
+                    onClick={() => {
+                      navigator.clipboard
+                        .writeText(data.ca)
+                        .then(() => toast.success("Copied to clipboard"))
+                        .catch(() => toast.error("Could not copy"));
+                    }}
+                    className="font-semibold cursor-pointer px-4 uppercase border border-[#9e782c] shadow-[2px_2px_0_#9e782c] active:shadow-[0.5px_0.5px_0_#9e782c] active:translate-x-[1px] active:translate-y-[1px] text-white active:scale-[0.99] transition-all ease-linear duration-100 hover:bg-yellow-500/80 active:bg-yellow-500/90 bg-yellow-500/70 text-[17px] py-2 rounded-xl"
+                  >
+                    COPY
+                  </button>
+                </div>
+                <code className="text-sm break-all">{data.ca}</code>
+                <div className="text-start mt-4">
+                  <ul className="list-disc list-inside text-left">
+                    {data.meta_info.map((info, idx) => (
+                      <li key={idx}>{info}</li>
+                    ))}
+                  </ul>
+                </div>
               </div>
             </div>
           </section>
@@ -90,4 +123,3 @@ export default function HomePage() {
     </motion.div>
   );
 }
-//:end
